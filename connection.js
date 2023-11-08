762095134baecbbbbd5ad987a2ea2b846e1c6404
paras <parassapariya212@gmail.com>
2023-11-08 18:33:57 +0530

    connection

---
+++ connection.js
@@ -0,0 +1,15 @@
+const { log } = require('console');
+const {MongoClient} =require('mongodb');
+const url="mongodb://localhost:27017";
+const con = new MongoClient(url);
+const databse = "ecom";
+
+
+async function getdata () {  
+    let result = await con.connect();
+    let db = result.db(databse);
+    return db.collection('admin');
+   
+}
+
+module.exports = getdata;
---
+++ index.js
@@ -0,0 +1,17 @@
+const getdata = require('./connection');
+// find data using promiss handale
+
+            // getdata().then((res)=>{
+            //     res.find().toArray().then((data)=>{
+            //         console.log(data);
+            //     })
+            // });
+
+//find data using async and await use
+
+            // let data = async ()=>{
+            //     let dt = await getdata();
+            //     let paras = await dt.find().toArray();
+            //     console.log(paras);
+            // };
+            // data();
