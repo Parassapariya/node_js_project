87c7ab01da1a48fac815678b2612f4f52c3ff4e0
paras <parassapariya212@gmail.com>
2023-11-09 18:32:38 +0530

    init

--- mongoess.js
+++ mongoess.js
@@ -1,11 +1,12 @@
 const mongooes = require('mongoose');
-await mongooes.connect("mongodb://localhost:27017/ecom");
+mongooes.connect("mongodb://localhost:27017/ecom");
+
+let eomschema = new mongooes.Schema({
+    username:String,
+    password:String
+});
+
 const main = async ()=>{
-   
-    let eomschema = new mongooes.Schema({
-        username:String,
-        password:String
-    });
     let ecommodel = mongooes.model('admin',eomschema);
     let data = new ecommodel({username:"paras",password:"123"});
     let result = await data.save();
